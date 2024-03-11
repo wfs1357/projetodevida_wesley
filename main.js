650d7c5f867c45095b46f0f9778b85cc7c52d8e9
wesley <106327635+wfs1357@users.noreply.github.com>
2024-03-11 12:16:06 -0300

    wwwdwdw

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ for(let i = 0; i < botoes.length; i++) {
      for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
      }
-     botoes[i].classList.remove("ativo");
+     botoes[i].classList.add("ativo");
   }
 }
